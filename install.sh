41dbbfc34c5b1366042eeae576a213436f35e286
Mahdi Mirzade <me@mahdym.ir>
2022-03-27 11:36:03 +0430

    Fix syntax, this is now POSIX shell complaint

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 case $1 in
-  *u*) sudo rm -r /usr/local/bin/fetch && printf "[*] fetch Uninstalled\n"; mkdir -p $HOME/.config/fetch;;
+  *u*) sudo rm -r /usr/local/bin/fetch && printf "[*] fetch Uninstalled\n"; mkdir -p "$HOME"/.config/fetch;;
   *i*) sudo cp -r fetch /usr/local/bin/fetch && printf "[*] fetch Installed\n";;
   *) printf "Usage: ./install.sh [ -i install | -u uninstall ]\n"
 esac
